223c85d4c58283f475c294d0dc40a5354effb69b
patternproject <patternproject@yahoo.com>
2017-05-19 17:12:26 +0500

    v 0 0 1: a) Basic Setup          b) Solved Ex 1

--- 2.Code/v 0 0 1.R
+++ 2.Code/v 0 0 1.R
@@ -1,8 +1,29 @@
 ## `````````````````````````````````````````````
 #### Read Me ####
 ## `````````````````````````````````````````````
-## This is the template file. Rename it as v 0 0 1.R in Code dir
-## And start coding
+
+## Attempting the excercise proposed above
+## From R-Bloggers:
+## Character Functions (Advanced)
+
+## Here are more details from the site:
+
+## This set of exercises will help you to help you improve your skills with
+## character functions in R. Most of the exercises are related with text mining,
+## a statistical technique that analyses text using statistics. If you find them
+## interesting I would suggest checking the library tm, this includes functions
+## designed for this task. There are many applications of text mining, a pretty
+## popular one is the ability to associate a text with his or her author, this
+## was how J.K.Rowling (Harry potter author) was caught publishing a new novel
+## series under an alias. Before proceeding, it might be helpful to look over
+## the help pages for the nchar, tolower, toupper, grep, sub and strsplit. Take
+## at the library stringr and the functions it includes such as str_sub.
+
+## SRC: https://www.r-bloggers.com/character-functions-advanced/
+
+## v 0 0 1: a) Basic Setup
+##          b) Solved Ex 1
+
 ## `````````````````````````````````````````````
 
 ## `````````````````````````````````````````````
@@ -10,8 +31,8 @@
 ## `````````````````````````````````````````````
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse)
-pacman::p_load(readxl)
-
+#pacman::p_load(readxl)
+pacman::p_load(stringr)
 
 ## `````````````````````````````````````````````
 
@@ -23,20 +44,6 @@ pacman::p_load(readxl)
 ## `````````````````````````````````````````````
 #### Global Settings ####
 ## `````````````````````````````````````````````
-
-## ````````````````````
-### Setting Working Dir
-# SRC: http://stackoverflow.com/questions/17605563/efficiently-convert-backslash-to-forward-slash-in-r
-# 1. At the prompt copy and paste windows path of project root folder
-x <- readline()
-# 2. 
-my.dir <- gsub("\\\\", "/", x)
-# 3. 
-setwd(my.dir)
-# 4. check
-getwd()
-## ````````````````````
-
 
 ## ````````````````````
 ### Set theme for plots
@@ -52,20 +59,80 @@ getwd()
 # so in order to load our data into R we need to first go into the “data” folder 
 # and then read in the data file, thus our call is “data/my.data.file.txt”
 
+## From the SRC web site:
+# Before starting the set of exercises run the following code lines :
+
+if (!'tm' %in% installed.packages()) install.packages('tm')
+library(tm)
+txt = system.file("texts", "txt", package = "tm")
+ovid = VCorpus(DirSource(txt, encoding = "UTF-8"),
+               readerControl = list(language = "lat"))
+OVID = c(data.frame(text=unlist(TEXT), stringsAsFactors = F))
+TEXT = lapply(ovid[1:5], as.character)
+TEXT1 = TEXT[[4]]
+
+temp = TEXT1
 ## `````````````````````````````````````````````
 
 
 ## `````````````````````````````````````````````
-#### Manipulate Data ####
+#### Set Up ####
 ## `````````````````````````````````````````````
+## 
+
+str(TEXT)
+str_length(TEXT) 
+temp = unlist(TEXT)
+str_length(temp)
+
+# I was wrongly assumping that we need to work with TEXT
+# but it is TEXT1 so the above set up is NOT required.
+
 ## `````````````````````````````````````````````
 
 
+
+#### Exercise 1 ####
 ## `````````````````````````````````````````````
-#### Plot 1 ####
+## Delete all the punctuation marks from TEXT1
+str_replace_all(temp,"[[:punct:]]","")
+## `````````````````````````````````````````````
+
+#### Exercise 2 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
+
+#### Exercise 3 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
+
+#### Exercise 4 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
+
+#### Exercise 5 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
+
+#### Exercise 6 ####
 ## `````````````````````````````````````````````
 ## `````````````````````````````````````````````
 
+#### Exercise 7 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
+
+#### Exercise 8 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
+
+#### Exercise 9 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
+
+#### Exercise 10 ####
+## `````````````````````````````````````````````
+## ````````````````````````````````````````````
 
 ## `````````````````````````````````````````````
 #### Clean up ####
